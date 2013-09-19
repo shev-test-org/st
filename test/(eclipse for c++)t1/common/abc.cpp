681fed1117beec0308419df359806b39c7ce6a61
ShevYan <YanDong_8212@163.com>
2013-09-19 13:24:44 +0800

    add a sample project by eclipse for c++.

---
+++ test/(eclipse for c++)t1/common/abc.cpp
@@ -0,0 +1,16 @@
+/*
+ * abc.cpp
+ *
+ *  Created on: Sep 19, 2013
+ *      Author: root
+ */
+
+//#include "abc.h"
+
+#include <cstdio>
+
+void fun() {
+	printf("fun..\n");
+}
+
+
---
+++ test/(eclipse for c++)t1/common/abc.h
@@ -0,0 +1,14 @@
+/*
+ * abc.h
+ *
+ *  Created on: Sep 19, 2013
+ *      Author: root
+ */
+
+#ifndef ABC_H_
+#define ABC_H_
+void fun();
+
+
+
+#endif /* ABC_H_ */
