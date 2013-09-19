ebd32886713db4add2a636f975d51cc9dae16b62
ShevYan <YanDong_8212@163.com>
2013-09-19 13:55:36 +0800

    add fuse helloworld to test.

---
+++ test/(eclipse for c++)t1/t1.cpp
@@ -0,0 +1,18 @@
+//============================================================================
+// Name        : t1.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C, Ansi-style
+//============================================================================
+
+
+#include <cstdio>
+
+#include "common/abc.h"
+
+int main(void) {
+	fun();
+	puts("Hello World!!!");
+	return 0;
+}
