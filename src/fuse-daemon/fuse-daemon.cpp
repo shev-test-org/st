782fb2c996fe01bfa3f74067958716873f621a2b
ShevYan <YanDong_8212@163.com>
2013-09-19 14:12:24 +0800

    create project structure and create a sample fuse-daemon project.

---
+++ src/fuse-daemon/fuse-daemon.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : fuse-daemon.cpp
+// Author      : ST
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C, Ansi-style
+//============================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hello World!!!");
+	return EXIT_SUCCESS;
+}
